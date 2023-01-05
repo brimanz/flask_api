0977712e5bf11f9cb39ac08d82f8db801865d5e7
jonathan briceño <brimanz2014@gmail.com>
2023-01-05 14:56:26 -0400

    first commit

---
+++ products.py
@@ -0,0 +1,17 @@
+products = [
+	{
+		"name": "laptop"
+		"price": 800,
+		"quantity": 4
+	}, 
+	{
+		"name": "mouse"
+		"price": 80,
+		"quantity": 40
+	}, 
+	{
+		"name": "board"
+		"price": 300,
+		"quantity": 14
+	}
+]
