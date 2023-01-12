c544165a5b4d76a786b768a0f33bba023a7abc3c
jonathan briceño <brimanz2014@gmail.com>
2023-01-12 15:30:14 -0400

    adding more basic API operations

--- app.py
+++ app.py
@@ -46,7 +46,35 @@ def addProduct():
 	products.append(new_product)
 	return (request.json)
 
+@app.route("/products/<string:product_name>", methods=["PUT"])
+def editProduct(product_name):
+	productFound = [product for product in products if product["name"] == product_name]	
 
+	if (len(productFound) > 0):
+		productFound[0]["name"] = request.json["name"]
+		productFound[0]["price"] = request.json["price"]
+		productFound[0]["quantity"] = request.json["quantity"]
+		return jsonify({
+			"message": "Producto Updated",
+			"product": productFound[0]
+		})
+	return jsonify({
+		"message": "Product not Found"
+	})
+
+@app.route("/products/<string:product_name>", methods=["DELETE"])
+def deleteProduct(product_name):
+	productFound = [product for product in products if product["name"] == product_name]	
+
+	if (len(productFound) > 0):
+		products.remove(productFound[0])
+		return jsonify({
+			"message": "Product Deleted!",
+			"products": products
+		})
+	return jsonify({
+		"message": "Product not Found"
+	})
 
 if __name__ == "__main__":
 	app.run(debug=True, port=4000)
